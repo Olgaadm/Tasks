dab2a92501a71cebbb286574b631c1d07ebe07f3
Olgaadm <olyaarsenych@gmail.com>
2022-04-08 10:56:08 +0200

    Added refactored implemantation of HashMap

--- src/com/hashMap/HashMap.java
+++ src/com/hashMap/HashMap.java
@@ -8,6 +8,7 @@ public class HashMap<E, V> implements Map<E, V> {
 
     private int itemsInSlots;
     private int deletedItems;
+    private int rehashedItems;
     private static final int INIT_CAPACITY = 11;
     private static final float LOAD_FACTOR = 0.75f;
     private int capacity = INIT_CAPACITY;
@@ -31,69 +32,55 @@ public class HashMap<E, V> implements Map<E, V> {
     public void put(E key, V value) {
         if (key == null || value == null) {
             throw new IllegalArgumentException("This hash map could not contain null values");
-        } else {
-            resize();
-            Entry<E, V> entry = new Entry<>(key, value);
-            int hashValue = hashFunction(key);
-            if (slots[hashValue] == null) {
-                slots[hashValue] = entry;
-            } else {
-                int i = hashValue;
-                while (!(slots[i] == null || slots[i].key.equals(key) || slots[i].deleted)) {
-                    i = (i + 1) % capacity;
-                }
-                slots[i] = entry;
-            }
+        }
+        resize();
+        Entry<E, V> entry = new Entry<>(key, value);
+        int i = searchSlot(key);
+        if (slots[i].key != entry.key) {
             itemsInSlots++;
         }
+        slots[i] = entry;
     }
 
     @Override
     public V get(E key) {
         assertParameters(key);
-        int hashValue = hashFunction(key);
-        var entry = slots[hashValue];
-        if (entry != null && entry.key.equals(key) && !entry.deleted)
-            return entry.value;
-        else {
-            int foundSlotIndex = searchSlot(key);
-            if (foundSlotIndex != -1)
-                return slots[foundSlotIndex].value;
-        }
-        return null;
+        int i = searchSlot(key);
+        assertFoundSlot(i);
+        return slots[i].value;
     }
 
 
     @Override
     public void remove(E key) {
         assertParameters(key);
-        int hashValue = hashFunction(key);
-        var entry = slots[hashValue];
-        if (entry == null || (entry.key.equals(key) && entry.deleted)) {
-            throw new NoSuchElementException("Item does not exist or is deleted already");
-        } else {
-            if (entry.key.equals(key)) {
-                slots[hashValue].deleted = true;
-            } else {
-                int foundSlotIndex = searchSlot(key);
-                if (foundSlotIndex != -1) {
-                    slots[foundSlotIndex].deleted = true;
-                }
-            }
-            deletedItems++;
-            resize();
+        int i = searchSlot(key);
+        assertFoundSlot(i);
+        slots[i].deleted = true;
+        deletedItems++;
+        itemsInSlots--;
+    }
+
+    private int searchSlot(E key) {
+        int i = hashFunction(key);
+        int k = capacity;
+        for (; k > 0 && slots[i] != null && !(slots[i].key.equals(key)); k--) {
+            i = (i + 1) % capacity;
         }
+        if (k != 0) {
+            return i;
+        } else
+            return -1;
     }
 
     private void resize() {
         if ((float) (itemsInSlots) / capacity > LOAD_FACTOR) {
             capacity = capacity * 2;
             rehash(capacity);
-        } else {
-            if (((float) (itemsInSlots + deletedItems) / capacity > LOAD_FACTOR)) {
-                rehash(capacity);
-            }
+        } else if (((float) (itemsInSlots + deletedItems) / capacity > LOAD_FACTOR)) {
+            rehash(capacity);
         }
+
     }
 
     private void rehash(int capacity) {
@@ -107,33 +94,33 @@ public class HashMap<E, V> implements Map<E, V> {
         }
     }
 
-    private int searchSlot(E key) {
-        for (int i = 0; i < capacity; i++) {
-            if (slots[i] != null && slots[i].key.equals(key) && !slots[i].deleted) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     private void assertParameters(E key) {
         if (slots == null || key == null) {
             throw new IllegalArgumentException("Either passed parameter is null or hashMap is empty");
         }
     }
 
-    private int hashFunction(E key) {
-        return key.hashCode() % capacity;
+    private void assertFoundSlot(int i) {
+        if (i == -1 || slots[i] == null || slots[i].deleted) {
+            throw new NoSuchElementException("Item does not exist or is deleted already");
+        }
     }
 
     public Entry<E, V>[] getSlots() {
-        return slots.clone();
+        Entry<E, V>[] copy = (Entry<E, V>[]) new Entry[capacity];
+        for (int i = 0; i < slots.length; i++) {
+            copy[i] = slots[i];
+        }
+        return copy;
+    }
+
+    private int hashFunction(E key) {
+        return key.hashCode() % capacity;
     }
 
     public int getCapacity() {
         return capacity;
     }
-
 
     public int getItemsInSlots() {
         return itemsInSlots;
--- src/com/hashMap/test/HashMapTest.java
+++ src/com/hashMap/test/HashMapTest.java
@@ -9,7 +9,7 @@ import java.util.NoSuchElementException;
 public class HashMapTest {
 
     @Test
-    public void testPut() {
+    private void testPut() {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(99, "test1");
         hashMap.put(1, "test3");
@@ -34,7 +34,7 @@ public class HashMapTest {
     }
 
     @Test
-    public void testCapacity() {
+    private void testCapacity() {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(1, "test1");
         hashMap.put(2, "test3");
@@ -55,7 +55,7 @@ public class HashMapTest {
     }
 
     @Test
-    public void testGetValue() {
+    private void testGetValue() {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(99, "test1");
         hashMap.put(1, "test3");
@@ -76,7 +76,7 @@ public class HashMapTest {
     }
 
     @Test
-    public void testRemove() {
+    private void testRemove() {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(99, "test1");
         hashMap.put(1, "test3");
@@ -84,7 +84,11 @@ public class HashMapTest {
         hashMap.put(98, "test9");
         hashMap.put(2, "test8");
         hashMap.remove(2);
-        Assert.assertNull(hashMap.get(2));
+        try {
+            hashMap.get(2);
+        } catch (NoSuchElementException e) {
+            Assert.assertEquals(e.getMessage(), "Item does not exist or is deleted already");
+        }
         hashMap.put(8, "test0");
         hashMap.put(2, "test999999998");
         hashMap.put(97, "test6");
@@ -98,7 +102,7 @@ public class HashMapTest {
     }
 
     @Test
-    public void testRemoveLargeAmount() {
+    private void testRemoveLargeAmount() {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(99, "test1");
         hashMap.put(1, "test3");
@@ -150,7 +154,7 @@ public class HashMapTest {
     }
 
     @Test
-    public void testRemoveWithResize() {
+    private void testRemoveWithResize() {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(99, "test1");
         hashMap.put(1, "test3");
@@ -185,5 +189,26 @@ public class HashMapTest {
         hashMap.remove(98);
         Assert.assertEquals(hashMap.getCapacity(), 22);
         Assert.assertEquals(hashMap.getItemsInSlots(), 1);
+    }
+
+    @Test
+    private void testPutLargeAmount() {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        for (int i = 0; i < 100; i++) {
+            hashMap.put(i, "test" + i);
+        }
+        Assert.assertEquals(hashMap.getItemsInSlots(), 100);
+        Assert.assertEquals(hashMap.getCapacity(), 176);
+
+        for (int i = 0; i < 100; i++) {
+            Assert.assertEquals(hashMap.get(i), "test" + i);
+        }
+        for (int i = 0; i < 50; i++) {
+            hashMap.remove(i);
+        }
+        Assert.assertEquals(hashMap.getItemsInSlots(), 50);
+        hashMap.put(99, "test101");
+        Assert.assertEquals(hashMap.getItemsInSlots(), 50);
+        Assert.assertEquals(hashMap.getCapacity(), 176);
     }
 }
